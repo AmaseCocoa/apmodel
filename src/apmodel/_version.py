0f056d01af21dfdc8f872ac05fd8c4aa757e7132
AmaseCocoa <cocoa@amase.cc>
2025-12-21 12:39:07 +0900

    feat(tests): setup codecov

--- src/apmodel/_version.py
+++ src/apmodel/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.4.5.post1.dev18+g137d651ce.d20251219'
-__version_tuple__ = version_tuple = (0, 4, 5, 'post1', 'dev18', 'g137d651ce.d20251219')
+__version__ = version = '0.4.5.post1.dev37+g1613af455.d20251221'
+__version_tuple__ = version_tuple = (0, 4, 5, 'post1', 'dev37', 'g1613af455.d20251221')
 
 __commit_id__ = commit_id = None
