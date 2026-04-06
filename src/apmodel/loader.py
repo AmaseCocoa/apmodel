cc6cfaa34a60e9310be5a8abf826717bd9d5399c
AmaseCocoa <cocoa@amase.cc>
2026-04-06 18:43:39 +0900

    fix: add context args

--- src/apmodel/loader.py
+++ src/apmodel/loader.py
@@ -1,12 +1,9 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Literal, TypedDict, get_type_hints
-
-from typing_extensions import Unpack
+from typing import TYPE_CHECKING, Any
 
 if TYPE_CHECKING:
     from apmodel.base import AS2Model
-
 
 from apmodel._vendor.type_mapping import TYPE_MAPPING
 from apmodel.inference import TypeInferencer
@@ -14,31 +11,15 @@ from apmodel.inference import TypeInferencer
 type_loader = TypeInferencer(TYPE_MAPPING)
 
 
-class ExtraArgs(TypedDict, total=False):
-    strict: bool | None
-    extra: Literal["allow", "ignore", "forbid"] | None
-    from_attributes: bool | None
-    by_alias: bool | None
-    by_name: bool | None
-
-
-def load(data: dict, context: dict | None = None, **kwargs: Unpack[ExtraArgs]) -> AS2Model | None:
+def load(
+    data: dict[str, Any], context: dict | None = None
+) -> AS2Model | None:
     if context is None:
         context = data
-
-    model_cls: type[AS2Model] | None = type_loader.infer(data, parent_context=context)
+        
+    model_cls = type_loader.infer(data, parent_context=context)
 
     if model_cls is not None:
-        allowed_keys = get_type_hints(ExtraArgs).keys()
-        
-        filtered = {
-            k: v for k, v in kwargs.items() 
-            if k in allowed_keys and v is not None
-        }
-
-        return model_cls.model_validate(data, context=context, **filtered)
+        return model_cls.model_validate(data, context=context)
 
     return None
-
-
-load({})
