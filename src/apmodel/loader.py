c200cfd8ad85c100bc2e8df26f90e66cd2bf0700
AmaseCocoa <cocoa@amase.cc>
2026-04-06 18:34:57 +0900

    fix: filter kwargs for load

--- src/apmodel/loader.py
+++ src/apmodel/loader.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Literal, TypedDict, get_type_hints
+
+from typing_extensions import Unpack
 
 if TYPE_CHECKING:
     from apmodel.base import AS2Model
@@ -11,15 +13,32 @@ from apmodel.inference import TypeInferencer
 
 type_loader = TypeInferencer(TYPE_MAPPING)
 
-def load(
-    data: dict, *args: object, context: dict | None = None, **kwargs: object
-) -> AS2Model | None:
+
+class ExtraArgs(TypedDict, total=False):
+    strict: bool | None
+    extra: Literal["allow", "ignore", "forbid"] | None
+    from_attributes: bool | None
+    by_alias: bool | None
+    by_name: bool | None
+
+
+def load(data: dict, context: dict | None = None, **kwargs: Unpack[ExtraArgs]) -> AS2Model | None:
     if context is None:
         context = data
-    
+
     model_cls: type[AS2Model] | None = type_loader.infer(data, parent_context=context)
 
     if model_cls is not None:
-        return model_cls.model_validate(data, *args, context=context, **kwargs)
+        allowed_keys = get_type_hints(ExtraArgs).keys()
+        
+        filtered = {
+            k: v for k, v in kwargs.items() 
+            if k in allowed_keys and v is not None
+        }
+
+        return model_cls.model_validate(data, context=context, **filtered)
 
     return None
+
+
+load({})
